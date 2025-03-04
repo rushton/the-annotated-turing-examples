f1036b4074d6b153a68b0d84e72c9062849b3f27
Nick Rushton <rushton.nicholas@gmail.com>
2025-03-04 09:02:34 -0800

    Separate tape class into it's own file for reuse

---
+++ tape.py
@@ -0,0 +1,44 @@
+class Tape:
+    def __init__(self):
+        self.tape = [" "]
+        self.position = 0
+
+    def move_left(self, spaces=1):
+        self.position -= spaces
+
+    def move_right(self, spaces=1):
+        self.position += spaces 
+        if len(self.tape) <= self.position:
+            for idx in range(spaces):
+                self.tape.append(" ")
+
+    def print_one(self):
+        self.tape[self.position] = "1"
+
+    def print_zero(self):
+        self.tape[self.position] = "0"
+
+    def print_schwa(self):
+        self.tape[self.position] = "e"
+
+    def print_x(self):
+        self.tape[self.position] = "x"
+
+    def read(self):
+        return self.tape[self.position]
+
+    def is_empty(self):
+        return self.tape[self.position] == " "
+
+    def is_one(self):
+        return self.tape[self.position] == "1"
+
+    def is_zero(self):
+        return self.tape[self.position] == "0"
+
+    def is_schwa(self):
+        return self.tape[self.position] == "e"
+
+    def print_tape(self):
+        print("| " + " | ".join(self.tape) + " |")
+        print((" " * 4 * self.position) + "  " + "^")
